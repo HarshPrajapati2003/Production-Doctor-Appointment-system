b60dd4268dabdac5593c9f6dd4c503699abc9c23
HarshPrajapati2003 <hrsdp2683@gmail.com>
2023-08-26 16:54:29 +0530

    First Commit

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect("mongodb://localhost:27017/covid");
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
